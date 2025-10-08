0f4e9a4c99ed3669126dd2ec62a6992e6332a1c8
Elloten Liu <elloten@EllotendeMacBook-Air.local>
2025-10-08 20:59:58 +0800

    update gmail x LLM script

---
+++ gmailApi_test.py
@@ -0,0 +1,56 @@
+############################
+# python測試gmail API是否成功
+###########################
+
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',  # 查看你的電子郵件和設定
+    'https://www.googleapis.com/auth/gmail.compose',   # 管理草稿和傳送電子郵件
+    'https://www.googleapis.com/auth/gmail.modify',    # 在 Gmail 帳戶中讀取、撰寫及傳送電子郵件
+    'https://mail.google.com/'                         # 閱讀、撰寫、傳送及永久刪除 Gmail 中的所有電子郵件
+]
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+if __name__ == '__main__':
+    main()
